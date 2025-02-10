4399df613f7e387252cdf5e4fd28d15ffd77260a
hemadriseai <hemadri@sierraedge.ai>
2025-02-10 11:46:37 +0530

    Add files via upload

---
+++ tlefextract.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3.9
+import re,os
+
+filee='/nas/nas_v1/PDK/tsmc_downloads/24012024/PRTF_Innovus_16nm_001_Cad_V17_1a/PR_tech/Cadence/LefHeader/Standard/HVH/PRTF_Innovus_N16_9M_2Xa1Xd3Xe2Z_UTRDL_9T_PODE.17_1a.tlef'
+
+fileid=open(filee)
+fcontent=fileid.read()
+fileid.close()
+r=open('tlefinfo.csv','w')
+r.write("layer,type,direction,mask,pitch,offset,minwidth,maxwidth,width,area,minenclosedarea\n")
+layers=re.findall('^LAYER\s+(\w+)\s*\n',fcontent,re.M)
+print(layers)
+for layer in layers:
+	data=re.findall(f'^LAYER\s+{layer}\s*\n.*?^END\s+{layer}',fcontent,re.S|re.M)[0]
+	ltype=re.findall('TYPE\s+(\S+)',data)[0]
+	if ltype.lower() == 'routing':
+		try:mask=re.findall('MASK\s+(\S+)',data)[0]
+		except:mask=''
+		try:direction=re.findall('DIRECTION\s+(\S+)',data)[0]
+		except:direction=''
+		try:pitch=re.findall('\s*PITCH\s+(.*);',data)[0]
+		except:pitch=''
+		try:offset=re.findall('\s*OFFSET\s+(.*);',data)[0]
+		except:offset=''
+		try:minwidth=re.findall('\s*MINWIDTH\s+(\S+)',data)[0]
+		except:minwidth=''
+		try:maxwidth=re.findall('\s*MAXWIDTH\s+(\S+)',data)[0]
+		except:maxwidth=''
+		try:width=re.findall('\s*WIDTH\s+(\S+)',data)[0]
+		except:width=''
+		try:area=re.findall('\s*AREA\s+(\S+)',data)[0]
+		except:area=''
+		try:minenclosedarea=re.findall('\s*MINENCLOSEDAREA\s+(\S+)',data)[0]
+		except:minenclosedarea=''
+		r.write(f'{layer},{ltype},{direction},{mask},{pitch},{offset},{minwidth},{maxwidth},{width},{area},{minenclosedarea}\n')
+
+r.close()
